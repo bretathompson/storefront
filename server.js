a5afb0b9cd12490bd34d57303bc37519bbe309fc
bretathompson <68876276+bretathompson@users.noreply.github.com>
2024-02-07 18:37:15 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require ('path');
 const bodyparser = require ("body-parser");
 const session = require ("express-session");
 const {v4: uuidv4} = require ("uuid");
-const {MongoClient} = require('mongodb');
+const MongoClient = require ("mongodb").MongoClient;
+
 
 const {router, setDatabase} = require('./router');
 const app = express ();
@@ -25,40 +26,46 @@ app.use (session({
     saveUninitialized: true
 }));
 
-MongoClient.connect(mongoURL, (err, client) => {
-  if (err) {
-    console.error('Error connecting to MongoDB:', err);
-  } else {
-    console.log('Connected to MongoDB');
-    const db = client.db();
+
+
+MongoClient.connect('mongodb://localhost:27017/storefront')
+.catch((err) => {
+  console.log(err);
+})
+.then((client) => {
+    console.log("Connected to MongoDB");
+    const db = client.db('storefront');
 
     // Log the number of items in the collection
-    const storeItemsCollection = db.collection('storeItems');
-    storeItemsCollection.insertMany([
-      { name: 'Item 1', price: 10 },
-      { name: 'Item 2', price: 20 },
-      // Add more items as needed
-    ], (insertErr, result) => {
-      if (insertErr) {
-        console.error('Error inserting store items:', insertErr);
-      } else {
-        console.log('Number of items inserted:', result.insertedCount);
+    const storeItemsCollection = db.collection("storeItems");
+    storeItemsCollection.insertMany(
+      [
+        { name: "Pizza", price: 10 },
+        { name: "Toy car", price: 20 },
+        { name: "Ball", price: 30 },
+        { name: "Board Game", price: 40 },
+      ],
+      (insertErr, result) => {
+        if (insertErr) {
+          console.error("Error inserting store items:", insertErr);
+        } else {
+          console.log("Number of items inserted:", result.insertedCount);
+        }
       }
-    });
+    );
 
-       // Set the database instance for the router
+    // Set the database instance for the router
     setDatabase(db);
 
-    app.use('/route', router);
+    app.use("/route", router);
 
-    app.get('/', (req, res) => {
-      res.render('base', { title: 'Login System' });
+    app.get("/", (req, res) => {
+      res.render("base", { title: "Login System" });
     });
 
     app.listen(port, () => {
       console.log("Listening to the server on http://localhost:3000");
     });
-  }
 });
 
 
